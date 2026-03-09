0ca5e3f355a06feccd260470acf7376e1dcd5911
johaoDev <johaobravoespinoza1501@gmail.com>
2026-03-09 13:10:04 -0500

    feat(ui): añade MainActivity y MainViewModel

--- app/src/main/java/com/senati/itanesturismo/ui/main/MainActivity.java
+++ app/src/main/java/com/senati/itanesturismo/ui/main/MainActivity.java
@@ -1,26 +1,47 @@
 package com.senati.itanesturismo.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.senati.itanesturismo.R;
+import com.senati.itanesturismo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_activity), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        setupObservers();
+        setupClickListeners();
+    }
+
+    private void setupObservers() {
+        viewModel.getUsername().observe(this, username -> {
+            String welcomeMessage = "¡Bienvenido de vuelta,\n" + username + "!";
+            binding.tvWelcomeMessage.setText(welcomeMessage);
+        });
+    }
+
+    // TODO: Reemplazar cuando los activities existan
+    private void setupClickListeners() {
+        binding.btnTouristPoints.setOnClickListener(v -> {
+            //Intent intent = new Intent(MainActivity.this, FakeTouristPointActivity.class);
+            //startActivity(intent);
+        });
+
+        binding.btnFavorites.setOnClickListener(v -> {
+            //Intent intent = new Intent(MainActivity.this, FakeFavoriteActivity.class);
+            //startActivity(intent);
         });
     }
 }
