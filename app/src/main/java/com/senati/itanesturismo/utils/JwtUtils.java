7867a38e333949ef33fb80ca3b4d6b75197c8897
johaoDev <johaobravoespinoza1501@gmail.com>
2026-03-12 11:55:05 -0500

    feat: añade nuevo método a JwtUtils para extraer el userId del token

--- app/src/main/java/com/senati/itanesturismo/utils/JwtUtils.java
+++ app/src/main/java/com/senati/itanesturismo/utils/JwtUtils.java
@@ -29,4 +29,24 @@ public final class JwtUtils {
         }
         return "Usuario";
     }
+
+    public static int extractUserIdFromJwt(String token) {
+        if (token == null || token.isEmpty()) return -1;
+        try {
+            String[] parts = token.split("\\.");
+            if (parts.length == 3) {
+                String payloadBase64 = parts[1];
+                byte[] decodedBytes = Base64.decode(payloadBase64, Base64.URL_SAFE);
+                String payloadJson = new String(decodedBytes, StandardCharsets.UTF_8);
+                JSONObject jsonObject = new JSONObject(payloadJson);
+                // En el JWT de la API, el ID viaja en el "sub" (subject)
+                if (jsonObject.has("sub")) {
+                    return Integer.parseInt(jsonObject.getString("sub"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
 }
